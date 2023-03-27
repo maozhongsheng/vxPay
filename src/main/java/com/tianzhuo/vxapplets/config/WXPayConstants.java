5012e69f9cbe1944a2467ad0c8ddad89ebb5e740
毛钟晟 <18636392387@163.com>
2023-03-27 09:56:33 +0800

    key

--- src/main/java/com/tianzhuo/vxapplets/config/WXPayConstants.java
+++ src/main/java/com/tianzhuo/vxapplets/config/WXPayConstants.java
@@ -2,8 +2,8 @@ package com.tianzhuo.vxapplets.config;
 
 public class WXPayConstants {
 
-    public static final String APP_ID = "wxe4d5046e8cc8ed29";//APP_ID
+    public static final String APP_ID = "wxaaf30549753c5290";//APP_ID
 
-    public static final String SECRET = "1c622a06c9438ce250a0602935a6c348";//SECRET_KEY
+    public static final String SECRET = "a463c92f8d9f603ca021bc83ff29e173";//SECRET_KEY
 
 }
